dd7565042681fc7dd1cec18d1775b784c0bc0308
Armand Silviu Gurgu <armandgurgu23@gmail.com>
2022-05-06 12:08:24 -0400

    Initialized skeleton setup for twitter scraper.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 import hydra
 from omegaconf import DictConfig
+from scrapers.twitter.twitter_scraper import main_twitter_scraper
 
 
 @hydra.main(config_path="config", config_name="config")
-def my_app(cfg: DictConfig) -> None:
-    print(cfg)
-    return
+def main(cfg: DictConfig) -> None:
+    if cfg.scraper.name == "twitter":
+        main_twitter_scraper(cfg)
+    else:
+        raise NotImplementedError(f"Scraper {cfg.scraper.name} not supported!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/scrapers/twitter/twitter_scraper.py
@@ -0,0 +1,9 @@
+from omegaconf import DictConfig
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def main_twitter_scraper(cfg: DictConfig) -> None:
+    logger.info("Running twitter scraper!")
+    return
